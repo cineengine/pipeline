88f039d3eaaf1e3027f8ebaf18e5ac64b0c60a9b
worrytron <rastermancer@gmail.com>
2015-05-19 08:37:01 -0400

    initial commit

---
+++ nuke/utils.py
@@ -0,0 +1,45 @@
+import nuke
+
+SWAP_PATHS = {
+	'R:/Projects/3013_ESPN_CFB_2015/04_Prod/ASSETS_LK/': 'Y:/Workspace/MASTER_PROJECTS/CFB_15/TOOLKIT/001_3D_ASSETS/',
+	'R:/Projects/3013_ESPN_CFB_2015/04_Prod/ASSETS_ESPN/002_3D_TEAMS': 'Y:/Workspace/MASTER_PROJECTS/CFB_15/TOOLKIT/002_3D_TEAMS'
+	'R:/Projects/3013_ESPN_CFB_2015/04_Prod/ASSETS_ESPN/000_ANIMATION': 'Y:/Workspace/MASTER_PROJECTS/CFB_15/PROJECTS/000_ANIMATION'
+}
+
+
+def getAllNodesByType(typ=''):
+
+	return_nodes = []
+
+	if typ == '':
+		return None
+
+	for n in nuke.allNodes():
+		if n.Class() == typ:
+			return_nodes.append(n)
+
+	return return_nodes
+
+
+def remapReadNodes():
+	read_nodes = getAllNodesByType('Read')
+
+	for rn in read_nodes:
+		read_path = rn['file'].getValue()
+
+		for k,v in SWAP_PATHS.iteritems():
+			if k in read_path:
+				print read_path.replace(k,v)
+				rn['file'].setValue(read_path.replace(k,v))
+
+
+def remapPrecompNodes():
+	precomp_nodes = getAllNodesByType('Precomp')
+
+	for pc in precomp_nodes:
+		pc_path = pc['file'].getValue()
+
+		for k,v in SWAP_PATHS.iteritems():
+			if k in pc_path:
+				print pc_path.replace(k,v)
+				pc['file'].setValue(pc_path.replace(k,v))
