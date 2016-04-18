e9ae9682ffddab239cf2afddf955e1223add25e5
worrytron <rastermancer@gmail.com>
2016-04-18 17:01:20 -0400

    limited documentation

--- AE/core.jsx
+++ AE/core.jsx
@@ -2,7 +2,6 @@
 
 var DATABASE_FOLDER = "Y:\\Workspace\\SCRIPTS\\pipeline\\database\\";
 
-
 function getItem(item_name, class_){
     /* Gets an item from the project window by name.  Looks for CompItem by default,
        but can be passed other objects (from the project window) to search for as well. */
@@ -57,17 +56,19 @@ function getProduction(){
 }
 
 function getProductionDatabase(production_){
+    // Searches the production database for a given production
     var database = File(DATABASE_FOLDER + "\\productions_db.json");
     database.open('r');
     var stream = database.read();
     stream = JSON.parse(stream);
-
+    // A default production database is always loaded
     default_db = stream['DEFAULT']
-
+    // .. and any production database found appends / merges with the default
     try {
         var prod_db = stream[production_];
         for (var key in default_db) prod_db[key] = default_db[key];
         prod_db['is_default'] = false;
+    // .. cleanup if no specific production database is found
     } catch(err) {
         prod_db = default_db;
         prod_db['is_default'] = true;
@@ -78,13 +79,14 @@ function getProductionDatabase(production_){
 }
 
 function getTeamDatabase(production_){
+    // Gets the team database for a given production
     var prod_db = getProductionDatabase(production_);
-
+    // if no team database is found
     if (prod_db['is_default'] === true){
         alert('Could not find team database for ' + production_ + '.')
         return false;
     }
-
+    // parse the JSON
     var database = File(DATABASE_FOLDER + prod_db['team_db'] + ".json");
     database.open('r');
     var stream = database.read();
@@ -95,6 +97,7 @@ function getTeamDatabase(production_){
 }
 
 function getTeam(production_, tricode){
+    // Searches a team DB for a given tricode
     var team_db = getTeamDatabase(production_);
     var team = null;
     for (var key in team_db){
@@ -107,36 +110,39 @@ function getTeam(production_, tricode){
 }
 
 function createTeamSwatches(){
+    // Creates team swatches for an entire production's worth of teams
     var prod_ = getProduction();
     var teams = getTeamDatabase(prod_);
     var all = '';
-
+    // Check for an existing TEAM_COLORS folder in the project window
     var team_folder = getItem('TEAM_COLORS', ItemCollection);
     if (team_folder === undefined){
         team_folder = app.project.items.addFolder('TEAM_COLORS');
     }
-
+    // Iterate over all teams
     for (var key in teams){
+        // Tricode & colors
         tricode   = key;
         primary   = teams[key]['primary'];
         secondary = teams[key]['secondary'];
         tertiary  = teams[key]['tertiary'];
-
+        // Comp defaults 
         name  = "{0}_COLORS".format(tricode);
         xres  = 1920;
         yres  = 1080;
-        par   = 1.0;
-        dur   = 60;
-        frate = 59.94;
+        par   = 1.0; // pixel aspect ratio
+        dur   = 60; // duration
+        frate = 59.94; // frame rate
+        // Create comp
         team_comp = app.project.items.addComp(name, xres, yres, par, dur, frate);
-
+        // Create 3 solids
         pri_solid = team_comp.layers.addSolid([0,0,0], "{0}_PRIMARY".format(tricode), xres/3, yres, par);
         sec_solid = team_comp.layers.addSolid([0,0,0], "{0}_SECONDARY".format(tricode), xres/3, yres, par);
         tri_solid = team_comp.layers.addSolid([0,0,0], "{0}_TERTIARY".format(tricode), xres/3, yres, par);
-
+        // Move the two outer solids to the side of the frame
         pri_solid.property("Position").setValue([320, 540]);
         tri_solid.property("Position").setValue([1600, 540]);
-
+        // Move the comp into the TEAM_COLORS folder
         team_comp.parentFolder = team_folder;
         setTeamColors(tricode);
     }
@@ -144,6 +150,8 @@ function createTeamSwatches(){
 }
 
 function setTeamColors(team_){
+    // Sets team colors on 3 solids in a comp, all of which are named with a team tricode
+    // container variables
     var comp;
 
     var primary;
@@ -153,24 +161,25 @@ function setTeamColors(team_){
     var primary_solid;
     var secondary_solid;
     var tertiary_solid;
-
+    // Get team comp & team data
     comp = getItem('{0}_COLORS'.format(team_));
     team = getTeam(getProduction(), team_);
 
     primary   = team['primary'];
     secondary = team['secondary'];
     tertiary  = team['tertiary'];
-
+    // Get color solids from comp
     primary_solid   = getLayer(comp, '{0}_PRIMARY'.format(team_))
     secondary_solid = getLayer(comp, '{0}_SECONDARY'.format(team_))
     tertiary_solid  = getLayer(comp, '{0}_TERTIARY'.format(team_))
-
+    // Change colors on solids
     primary_solid.source.mainSource.color = convertColor(primary);
     secondary_solid.source.mainSource.color = convertColor(secondary);
     tertiary_solid.source.mainSource.color = convertColor(tertiary);
 }
 
 function convertColor(color){
+    // Converts color arrays from integer to float
     var r = color[0];
     var g = color[1];
     var b = color[2];
@@ -182,6 +191,7 @@ function convertColor(color){
 }
 
 String.prototype.format = function() {
+    // Adds a .format() method to the String prototype, similar to python
     var formatted = this;
     for (var i = 0; i < arguments.length; i++) {
         var regexp = new RegExp('\\{'+i+'\\}', 'gi');
